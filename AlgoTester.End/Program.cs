c7ae00eb64045d8bb3a51a26827d2e96f0e9e8d9
Vladyslav Taraban <taraban@fedandburk.com>
2023-09-11 23:17:05 +0300

    Added project for End Problem

---
+++ AlgoTester.End/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+using static AlgoTester.Helpers.ConsoleHelper;
+
+namespace AlgoTester.End
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
